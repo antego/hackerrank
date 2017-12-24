6ba6d8f45cc1393f0a23838fbbca68be38c2f605
antego <antego@users.noreply.github.com>
2017-12-24 11:15:15 +0300

    merge sort

--- bigsort.py
+++ bigsort.py
@@ -3,45 +3,60 @@
 import sys
 
 unsorted = [
-    "4",
-    "11",
-    "110",
-    "12",
-    "11000"]
+    "31415926535897932384626433832795",
+    "1",
+    "3",
+    "10",
+    "3",
+    "5"]
+
+# inplace non-recursive merge-sort
 
 
-# quick sort without recursion
-def sort_array(arr, start, end):
-    if (end - start) < 2:
-        return
-    pivot_idx = int((end - start) / 2)
-    pivot = arr[pivot_idx]
-    cur_el = start
-    while cur_el < end:
-        cur_str = arr[cur_el]
+def lt(left, right):
+    if len(left) == len(right):
+        return left < right
 
-        if cur_el < pivot_idx and not (len(cur_str) < len(pivot)) and (len(cur_str) > len(pivot) or cur_str > pivot):
-            arr.insert(pivot_idx, arr.pop(cur_el))
-            pivot_idx -= 1
-        elif cur_el > pivot_idx and not (len(cur_str) > len(pivot)) and (len(cur_str) < len(pivot) or cur_str < pivot):
-            arr.insert(pivot_idx, arr.pop(cur_el))
-            pivot_idx += 1
+    if len(left) > len(right):
+        return False
+    else:
+        return True
+
+
+def merge(arr, start, mid, end):
+    buff = arr[start:mid]
+    left_idx = 0
+    right_idx = mid
+    cur_idx = start
+    while cur_idx != right_idx:
+        cur_left = buff[left_idx]
+        if right_idx == end or lt(cur_left, arr[right_idx]):
+            arr[cur_idx] = cur_left
+            left_idx += 1
         else:
-            cur_el += 1
-    return pivot_idx
+            arr[cur_idx] = arr[right_idx]
+            right_idx += 1
+        cur_idx += 1
 
+boundaries = []
+last_end = 0
+for idx in range(len(unsorted)-1):
+    if lt(unsorted[idx+1], unsorted[idx]):
+        boundaries.append((last_end, idx+1))
+        last_end = idx+1
+boundaries.append((last_end, len(unsorted)))
 
-stack = []
-pivot = sort_array(unsorted, 0, len(unsorted))
-stack.append((0, pivot))
-stack.append((pivot + 1, len(unsorted)))
-while len(stack) > 0:
-    start, end = stack.pop()
-    if end - start < 2:
-        continue
-    pivot = sort_array(unsorted, start, end)
-    stack.append((start, pivot))
-    stack.append((pivot + 1, end))
+while len(boundaries) != 1:
+    new_boundaries = []
+    for i in range(0, len(boundaries)-1, 2):
+        (start1, end1) = boundaries[i]
+        (start2, end2) = boundaries[i+1]
+        merge(unsorted, start1, end1, end2)
+        new_boundaries.append((start1, end2))
+    if len(boundaries) % 2 != 0:
+        new_boundaries.append(boundaries[-1])
+    boundaries = new_boundaries
+
 
 for i in unsorted:
     print(i)
