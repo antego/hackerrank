84d8148e0296798c91fa4fd911bdf29a0469370e
antego <antego@users.noreply.github.com>
2017-12-29 21:27:41 +0300

    counting swaps done

--- javarank/src/mergesort/Solution.java
+++ javarank/src/mergesort/Solution.java
@@ -5,23 +5,41 @@ import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = {1,3,5,7,2,4,6};
-        mergeSort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {2, 1, 4, 3, 6, 5};
+        int[] aux = new int[arr.length];
+        for (int i = 0; i < aux.length; i++) {
+            aux[i] = i;
+        }
+        mergeSort(arr, aux);
+
+        int swaps = 0;
+        for (int i = 0; i < aux.length; i++) {
+            if (aux[i] != i) {
+                int k = i;
+                do {
+                    int buf = k;
+                    k = aux[k];
+                    aux[buf] = buf;
+                    swaps++;
+                } while (k != i);
+                swaps--;
+            }
+        }
+        System.out.println(swaps);
     }
 
-    private static void mergeSort(int[] arr) {
-        while (!mergeInPlace(arr)) {};
+    private static void mergeSort(int[] arr, int[] aux) {
+        while (!mergeInPlace(arr, aux)) {};
     }
 
-    private static boolean mergeInPlace(int arr[]) {
+    private static boolean mergeInPlace(int arr[], int aux[]) {
         boolean foundSecond = false;
         int first = 0;
         int second = 0;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < arr[i-1]) {
                 if (foundSecond) {
-                    mergeParts(arr, first, second, i);
+                    mergeParts(arr, first, second, i, aux);
                     foundSecond = false;
                     first = i;
                 } else {
@@ -31,16 +49,21 @@ public class Solution {
             }
         }
         if (foundSecond) {
-            mergeParts(arr, first, second, arr.length);
+            mergeParts(arr, first, second, arr.length, aux);
         }
         return first == 0;
     }
 
 
-    private static void mergeParts(int[] arr, int start,  int mid, int end) {
+    private static void mergeParts(int[] arr, int start, int mid, int end, int[] aux) {
         int[] buf = new int[mid-start];
         for (int i = 0; i < buf.length; i++) {
             buf[i] = arr[start+i];
+        }
+
+        int[] auxBuf = new int[buf.length];
+        for (int i = 0; i < buf.length; i++) {
+            auxBuf[i] = aux[start+i];
         }
 
         int b = 0;
@@ -49,15 +72,19 @@ public class Solution {
         while (i < end) {
             if (b == buf.length) {
                 arr[i] = arr[r];
+                aux[i] = aux[r];
                 r++;
             } else if (r == end) {
                 arr[i] = buf[b];
+                aux[i] = auxBuf[b];
                 b++;
             } else if (arr[r] < buf[b]) {
+                aux[i] = aux[r];
                 arr[i] = arr[r];
                 r++;
             } else {
                 arr[i] = buf[b];
+                aux[i] = auxBuf[b];
                 b++;
             }
             i++;
